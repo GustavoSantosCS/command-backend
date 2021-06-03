ae8d08f4d951807cc5d2294ff5387e063bcefb96
Gustavo Santos <gustavosantos.development@gmail.com>
2021-06-02 22:11:04 -0300

    test(db-add-account): hasher password

--- tests/data/implementations/account/db-add-account-test.spec.ts
+++ tests/data/implementations/account/db-add-account-test.spec.ts
@@ -4,6 +4,7 @@ import {
   AddAccountRepository,
   SearchAccountByEmailRepository
 } from '@/data/protocols';
+import { Hasher } from '@/data/protocols/cryptography';
 import { EmailAlreadyUseError } from '@/domain/errors';
 import { Account } from '@/domain/models';
 import { InternalServerError } from '@/presentation/errors';
@@ -14,8 +15,25 @@ import {
   SearchAccountByEmailRepositorySpy
 } from '@tests/infra/db/account';
 
+class HasherSpy implements Hasher {
+  parameters: string;
+  error: Error;
+  return: string = 'hash';
+
+  throwsError() {
+    this.error = new Error('any_message');
+  }
+
+  async hash(plaintext: string): Promise<string> {
+    this.parameters = plaintext;
+    if (this.error) throw this.error;
+    return this.return;
+  }
+}
+
 let sut: DBAddAccount;
 let idGeneratorSpy: IDGenerator;
+let hasherSpy: Hasher;
 let searchByEmailRepositorySpy: SearchAccountByEmailRepository;
 let addAccountRepositorySpy: AddAccountRepository;
 let newAccount: Omit<Account, 'id'>;
@@ -26,12 +44,32 @@ describe('Test Unit: DBAddAccount', () => {
     idGeneratorSpy = new IdGeneratorSpy();
     searchByEmailRepositorySpy = new SearchAccountByEmailRepositorySpy();
     addAccountRepositorySpy = new AddAccountRepositorySpy();
+    hasherSpy = new HasherSpy();
 
     sut = new DBAddAccount(
       idGeneratorSpy,
+      hasherSpy,
       searchByEmailRepositorySpy,
       addAccountRepositorySpy
     );
+  });
+
+  it('should call SearchAccountByEmailRepository with the correct values', async () => {
+    const spy = searchByEmailRepositorySpy as SearchAccountByEmailRepositorySpy;
+
+    await sut.add(newAccount);
+
+    expect(spy.parameters).toEqual(newAccount.email);
+  });
+
+  it('should return EmailAlreadyUseError if email already use', async () => {
+    const spy = searchByEmailRepositorySpy as SearchAccountByEmailRepositorySpy;
+    spy.return = spy.returns.right;
+
+    const response = await sut.add(newAccount);
+
+    expect(response.isLeft()).toBeTruthy();
+    expect(response.value).toEqual(new EmailAlreadyUseError(newAccount.email));
   });
 
   it('should call generator id', async () => {
@@ -52,18 +90,11 @@ describe('Test Unit: DBAddAccount', () => {
     expect(response.value).toEqual(new InternalServerError('any_message'));
   });
 
-  it('should call SearchAccountByEmailRepository with the correct values', async () => {
-    const spy = searchByEmailRepositorySpy as SearchAccountByEmailRepositorySpy;
+  it('should call Hasher with the correct values', async () => {
+    const spy = hasherSpy as HasherSpy;
 
     await sut.add(newAccount);
 
-    expect(spy.parameters).toEqual(newAccount.email);
-  });
-
-  it('should return EmailAlreadyUseError if email already use', async () => {
-    const response = await sut.add(newAccount);
-
-    expect(response.isLeft()).toBeTruthy();
-    expect(response.value).toEqual(new EmailAlreadyUseError(newAccount.email));
+    expect(spy.parameters).toEqual(newAccount.password);
   });
 });
