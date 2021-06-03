8edf87875cc1ffa8393a6b6e1e7be240f7434266
Gustavo Santos <gustavosantos.development@gmail.com>
2021-06-03 15:58:44 -0300

    test: add email-validator

---
+++ tests/validation/validators/email-validation-test.spec.ts
@@ -0,0 +1,37 @@
+import faker from 'faker';
+import { EmailValidator } from '@/validation/validators';
+import { IsNotEmailError, MissingParamError } from '@/validation/errors';
+
+let fieldLabel: string;
+let sut: EmailValidator;
+
+describe('EmailValidator', () => {
+  beforeEach(() => {
+    fieldLabel = faker.database.column();
+    sut = new EmailValidator(fieldLabel);
+  });
+
+  test('Should return error if email is invalid', () => {
+    const fieldValue = faker.random.word();
+
+    const result = sut.validate({ [fieldLabel]: fieldValue });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toEqual(new IsNotEmailError(fieldLabel, fieldValue));
+  });
+
+  test('Should return true if email is valid', () => {
+    const fieldValue = faker.internet.email();
+
+    const result = sut.validate({ [fieldLabel]: fieldValue });
+
+    expect(result.isRight()).toBeTruthy();
+  });
+
+  test('Should return falsy if email is empty', () => {
+    const error = sut.validate({ [fieldLabel]: null });
+
+    expect(error.isLeft()).toBeTruthy();
+    expect(error.value).toEqual(new MissingParamError(fieldLabel));
+  });
+});
